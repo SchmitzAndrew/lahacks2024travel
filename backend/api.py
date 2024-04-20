fa09200884b63315636a21724a079ea321847c1d
Andrew Masek <portoaj@users.noreply.github.com>
2024-04-20 15:37:01 -0700

    Add city to api

--- backend/api.py
+++ backend/api.py
@@ -29,6 +29,7 @@ def get_places():
         attraction_details['name'] = attraction['name']
         attraction_details['description'] = 'Placeholder description'
         attraction_details['image_url'] = attraction['image_url']
+        attraction_details['city'] = attraction['city']
         places.append(attraction_details)
     result = dict()
     result['success'] = success
