4c3fdf20cd580fee14f2aa13564548e8e722b058
Andrew Masek <portoaj@users.noreply.github.com>
2024-04-20 13:47:24 -0700

    Added multithreaded descriptions generation

--- backend/api.py
+++ backend/api.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify, request
-from utils import get_top_attractions
+from utils import get_top_attractions, get_gemini_result
 from flask_cors import CORS, cross_origin
-
+import concurrent.futures
 import os
+
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -32,29 +33,36 @@ def get_places():
     result['places'] = places
     return jsonify(result)
 
+def get_description_obj(place):
+        id = place['id']
+        name = place['name']
+        description = get_gemini_result(f"Create a description for the following place in plain text without markdown or extra newlines: {name}")
+        return id, description
+    
 @cross_origin()
 @app.route('/placedescriptions', methods=['POST'])
 def get_place_descriptions():
-    latitude = request.args.get('lat')
-    longitude = request.args.get('long')
-    print(latitude, longitude)
-    attractions = get_top_attractions(latitude, longitude)
+    print('started')
+    places = request.json['places']
 
     success = True
-    places = []
-    for i, attraction in enumerate(attractions):
-        attraction_details = dict()
-        attraction_details['id'] = i
-        attraction_details['name'] = attraction['name']
-        attraction_details['description'] = 'Placeholder description'
-        attraction_details['image_url'] = attraction['image_url']
-        places.append(attraction_details)
+    descriptions = []
+
+    # Ask Gemini for descriptions of each place in parallel
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        results = executor.map(get_description_obj, places)
+
+        for id, description in results:
+            description_obj = {'id': id, 'description': description}
+            descriptions.append(description_obj)
+
     result = dict()
     result['success'] = success
-    result['places'] = places
+    result['descriptions'] = descriptions
+    
     return jsonify(result)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    port = int(os.environ.get('PORT', 5000)) 
+    app.run(host='0.0.0.0', port=port, debug=bool(os.environ.get('BACKEND_DEBUG', False)))
 
