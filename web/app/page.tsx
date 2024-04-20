8e03f77c52b5889b34fe79745cebd371c564ec31
SchmitzAndrew <schmitzandrew03@gmail.com>
2024-04-20 00:25:07 -0700

    VERY IMPORTANT COMMIT

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -4,6 +4,9 @@ export default function Index() {
   return (
     <main className="">
       <Landing />
+      <div className="px-4">
+      <iframe src="https://open.spotify.com/embed/track/0F13K9dwYH2zpTWiR8d628" width="100%" height="380" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+      </div>
     </main>
   );
 }
