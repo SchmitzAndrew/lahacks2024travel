f03fd212c8db24e5dc16d145a837228fbc97e6b8
SchmitzAndrew <schmitzandrew03@gmail.com>
2024-04-20 02:58:48 -0700

    add location button and input box

--- web/app/guide/page.tsx
+++ web/app/guide/page.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from 'react';
 import Container from "@/components/ui/Container";
 import AnimatedButton from "@/components/ui/AnimatedButton";
 
+import MagnifyingGlassIcon from "@heroicons/react/24/outline/MagnifyingGlassIcon";
+
 export default function Guide() {
     // State variables to store latitude and longitude
     const [latitude, setLatitude] = useState<number | null>(null);
@@ -62,33 +64,49 @@ export default function Guide() {
         <>
             <div className="bg-gray-50">
                 <Container>
-                    <>
-                        <div>
-                            <h2> Your Starting Location </h2>
+                    <div>
+                        <div className="flex flex-col items-center">
+                            <div className="pt-6 pb-3">
+                                <AnimatedButton >
+                                    Use My Location 🧭
+                                </AnimatedButton>
+                            </div>
+                            <p className=" text-gray-700">-or-</p>
                             <div>
-                                <p>Latitude: {latitude}</p>
-                                <p>Longitude: {longitude}</p>
+
+                                <div className="relative mt-2 rounded-md shadow-sm">
+                                    <input
+                                        type="text"
+                                        name="address"
+                                        id="address"
+                                        className="block w-full rounded-md border-0 py-1.5 pr-6 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        placeholder="enter your address"
+                                    />
+                                    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+                                        <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <h2 className="text-2xl font-bold pt-6 text-slate-900"> Nearby Places </h2>
+                                {places === null ? (
+                                    <p>Loading places...</p>
+                                ) : places.length > 0 ? (
+                                    <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 gap-x-8">
+                                        {places.map((place, index) => (
+                                            <li key={index} className="mb-4 rounded-lg bg-white shadow p-3">
+                                                <h3 className="text-lg font-semibold text-slate-900">{place.name}</h3>
+                                                <img src={place.image_url} alt={place.name} className="w-full h-auto rounded-xl" />
+                                                <p>{place.description}</p>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                ) : (
+                                    <p>No places found.</p>
+                                )}
                             </div>
                         </div>
-                        <div>
-                            <h2 className="text-2xl font-bold pt-6 text-slate-900"> Nearby Places </h2>
-                            {places === null ? (
-                                <p>Loading places...</p>
-                            ) : places.length > 0 ? (
-                                <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 gap-x-8">
-                                    {places.map((place, index) => (
-                                        <li key={index} className="mb-4 rounded-lg bg-white shadow p-3">
-                                            <h3 className="text-lg font-semibold text-slate-900">{place.name}</h3>
-                                            <img src={place.image_url} alt={place.name} className="w-full h-auto rounded-xl" />
-                                            <p>{place.description}</p>
-                                        </li>
-                                    ))}
-                                </ul>
-                            ) : (
-                                <p>No places found.</p>
-                            )}
-                        </div>
-                    </>
+                    </div>
                 </Container>
             </div>
         </>
