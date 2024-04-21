a12e2b81f669adaf82df57c3562e90f83d0c3648
SchmitzAndrew <schmitzandrew03@gmail.com>
2024-04-21 02:37:49 -0700

    landing page changes

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -5,7 +5,7 @@ export default function Index() {
     <main className="">
       <Landing />
       <div className="px-4">
-      <iframe src="https://open.spotify.com/embed/track/0F13K9dwYH2zpTWiR8d628" width="100%" height="380" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+      <iframe src="https://open.spotify.com/embed/track/1pKYYY0dkg23sQQXi0Q5zN" width="100%" height="380" frameBorder="0"  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
       </div>
     </main>
   );
